824e5fdf4c5e6702279878be6da69729e9dc8041
datagirl <datagirlcmr@gmail.com>
2020-08-17 09:07:17 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
   get 'profile', to: 'users#show'
+  put '/edit-profile', to: 'users#update'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
