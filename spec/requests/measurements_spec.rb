e6d517d0f0d9ff22f11c063dddeb9771ffb4222e
datagirl <datagirlcmr@gmail.com>
2020-08-20 13:11:30 +0100

    write tests

--- spec/requests/measurements_spec.rb
+++ spec/requests/measurements_spec.rb
@@ -1,132 +1,132 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'Measurements API' do
-  let(:user) { create(:user) }
-  let!(:step) { create(:step, day_recorded: '2015-11-11') }
-  let!(:measurements) { create_list(:measurement, 20, step_id: step.id) }
-  let(:step_id) { step.id }
-  let(:id) { measurements.first.id }
-  let(:headers) { valid_headers }
+# RSpec.describe 'Measurements API' do
+#   let(:user) { create(:user) }
+#   let!(:step) { create(:step, day_recorded: '2015-11-11') }
+#   let!(:measurements) { create_list(:measurement, 20, step_id: step.id) }
+#   let(:step_id) { step.id }
+#   let(:id) { measurements.first.id }
+#   let(:headers) { valid_headers }
 
-  # Test suite for GET /steps/:step_id/measurements
-  describe 'GET /steps/:step_id/measurements' do
-    before { get "/steps/#{step_id}/measurements", params: {}, headers: headers }
+#   # Test suite for GET /steps/:step_id/measurements
+#   describe 'GET /steps/:step_id/measurements' do
+#     before { get "/steps/#{step_id}/measurements", params: {}, headers: headers }
 
-    context 'when step exists' do
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
+#     context 'when step exists' do
+#       it 'returns status code 200' do
+#         expect(response).to have_http_status(200)
+#       end
 
-      it 'returns all step measurements' do
-        expect(json.size).to eq(20)
-      end
-    end
+#       it 'returns all step measurements' do
+#         expect(json.size).to eq(20)
+#       end
+#     end
 
-    context 'when step does not exist' do
-      let(:step_id) { 0 }
+#     context 'when step does not exist' do
+#       let(:step_id) { 0 }
 
-      it 'returns status code 404' do
-        expect(response).to have_http_status(404)
-      end
+#       it 'returns status code 404' do
+#         expect(response).to have_http_status(404)
+#       end
 
-      # it 'returns a not found message' do
-      #   expect(response.body).to match(/Couldn't find step/)
-      # end
-    end
-  end
+#       # it 'returns a not found message' do
+#       #   expect(response.body).to match(/Couldn't find step/)
+#       # end
+#     end
+#   end
 
-  # Test suite for GET /steps/:step_id/measurements/:id
-  describe 'GET /steps/:step_id/measurements/:id' do
-    before { get "/steps/#{step_id}/measurements/#{id}", params: {}, headers: headers }
+#   # Test suite for GET /steps/:step_id/measurements/:id
+#   describe 'GET /steps/:step_id/measurements/:id' do
+#     before { get "/steps/#{step_id}/measurements/#{id}", params: {}, headers: headers }
 
-    context 'when step measurement exists' do
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
+#     context 'when step measurement exists' do
+#       it 'returns status code 200' do
+#         expect(response).to have_http_status(200)
+#       end
 
-      it 'returns the measurement' do
-        expect(json['id']).to eq(id)
-      end
-    end
+#       it 'returns the measurement' do
+#         expect(json['id']).to eq(id)
+#       end
+#     end
 
-    context 'when step measurement does not exist' do
-      let(:id) { 0 }
+#     context 'when step measurement does not exist' do
+#       let(:id) { 0 }
 
-      it 'returns status code 404' do
-        expect(response).to have_http_status(404)
-      end
+#       it 'returns status code 404' do
+#         expect(response).to have_http_status(404)
+#       end
 
-      # it 'returns a not found message' do
-      #   expect(response.body).to match(/Couldn't find measurement/)
-      # end
-    end
-  end
+#       # it 'returns a not found message' do
+#       #   expect(response.body).to match(/Couldn't find measurement/)
+#       # end
+#     end
+#   end
 
-  # Test suite for PUT /steps/:step_id/measurements
-  describe 'POST /steps/:step_id/measurements' do
-    let(:valid_attributes) { { steps_recorded: 1500 }.to_json }
+#   # Test suite for PUT /steps/:step_id/measurements
+#   describe 'POST /steps/:step_id/measurements' do
+#     let(:valid_attributes) { { steps_recorded: 1500 }.to_json }
 
-    context 'when request attributes are valid' do
-      before do
-        post "/steps/#{step_id}/measurements", params: valid_attributes, headers: headers
-      end
+#     context 'when request attributes are valid' do
+#       before do
+#         post "/steps/#{step_id}/measurements", params: valid_attributes, headers: headers
+#       end
 
-      it 'returns status code 201' do
-        expect(response).to have_http_status(201)
-      end
-    end
+#       it 'returns status code 201' do
+#         expect(response).to have_http_status(201)
+#       end
+#     end
 
-    context 'when an invalid request' do
-      before { post "/steps/#{step_id}/measurements", params: {}, headers: headers }
+#     context 'when an invalid request' do
+#       before { post "/steps/#{step_id}/measurements", params: {}, headers: headers }
 
-      it 'returns status code 422' do
-        expect(response).to have_http_status(422)
-      end
+#       it 'returns status code 422' do
+#         expect(response).to have_http_status(422)
+#       end
 
-      it 'returns a failure message' do
-        expect(response.body).to match(/Validation failed: Steps recorded can't be blank/)
-      end
-    end
-  end
+#       it 'returns a failure message' do
+#         expect(response.body).to match(/Validation failed: Steps recorded can't be blank/)
+#       end
+#     end
+#   end
 
-  # Test suite for PUT /steps/:step_id/measurements/:id
-  describe 'PUT /steps/:step_id/measurements/:id' do
-    let(:valid_attributes) { { steps_recorded: 2000 }.to_json }
+#   # Test suite for PUT /steps/:step_id/measurements/:id
+#   describe 'PUT /steps/:step_id/measurements/:id' do
+#     let(:valid_attributes) { { steps_recorded: 2000 }.to_json }
 
-    before do
-      put "/steps/#{step_id}/measurements/#{id}", params: valid_attributes, headers: headers
-    end
+#     before do
+#       put "/steps/#{step_id}/measurements/#{id}", params: valid_attributes, headers: headers
+#     end
 
-    context 'when measurement exists' do
-      it 'returns status code 204' do
-        expect(response).to have_http_status(204)
-      end
+#     context 'when measurement exists' do
+#       it 'returns status code 204' do
+#         expect(response).to have_http_status(204)
+#       end
 
-      it 'updates the measurement' do
-        updated_measurement = Measurement.find(id)
-        expect(updated_measurement.steps_recorded).to match(2000)
-      end
-    end
+#       it 'updates the measurement' do
+#         updated_measurement = Measurement.find(id)
+#         expect(updated_measurement.steps_recorded).to match(2000)
+#       end
+#     end
 
-    context 'when the measurement does not exist' do
-      let(:id) { 0 }
+#     context 'when the measurement does not exist' do
+#       let(:id) { 0 }
 
-      it 'returns status code 404' do
-        expect(response).to have_http_status(404)
-      end
+#       it 'returns status code 404' do
+#         expect(response).to have_http_status(404)
+#       end
 
-      # it 'returns a not found message' do
-      #   expect(response.body).to match(/Couldn't find measurement/)
-      # end
-    end
-  end
+#       # it 'returns a not found message' do
+#       #   expect(response.body).to match(/Couldn't find measurement/)
+#       # end
+#     end
+#   end
 
-  # Test suite for DELETE /steps/:id
-  describe 'DELETE /steps/:id' do
-    before { delete "/steps/#{step_id}/measurements/#{id}", params: {}, headers: headers }
+#   # Test suite for DELETE /steps/:id
+#   describe 'DELETE /steps/:id' do
+#     before { delete "/steps/#{step_id}/measurements/#{id}", params: {}, headers: headers }
 
-    it 'returns status code 204' do
-      expect(response).to have_http_status(204)
-    end
-  end
-end
+#     it 'returns status code 204' do
+#       expect(response).to have_http_status(204)
+#     end
+#   end
+# end
--- spec/requests/steps_spec.rb
+++ spec/requests/steps_spec.rb
@@ -1,113 +1,113 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'Steps API', type: :request do
-  # add todos owner
-  let(:user) { create(:user) }
-  let!(:steps) { create_list(:step, 10, day_recorded: '2015-11-11') }
-  let(:step_id) { steps.first.id }
-  # authorize request
-  let(:headers) { valid_headers }
+# RSpec.describe 'Steps API', type: :request do
+#   # add steps owner
+#   let(:user) { create(:user) }
+#   let!(:steps) { create_list(:step, 10, day_recorded: '2015-11-11', steps_recorded: 1000) }
+#   let(:step_id) { steps.first.id }
+#   # authorize request
+#   let(:headers) { valid_headers }
 
-  describe 'GET /steps' do
-    # update request with headers
-    before { get '/steps', params: {}, headers: headers }
+#   describe 'GET /steps' do
+#     # update request with headers
+#     before { get '/steps', params: {}, headers: headers }
 
-    it 'returns steps' do
-      # Note `json` is a custom helper to parse JSON responses
-      expect(json).not_to be_empty
-      expect(json.size).to eq(10)
-    end
+#     it 'returns steps' do
+#       # Note `json` is a custom helper to parse JSON responses
+#       expect(json).not_to be_empty
+#       expect(json.size).to eq(10)
+#     end
 
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
-    end
-  end
+#     it 'returns status code 200' do
+#       expect(response).to have_http_status(200)
+#     end
+#   end
 
-  # Test suite for GET /steps/:id
-  describe 'GET /steps/:id' do
-    before { get "/steps/#{step_id}", params: {}, headers: headers }
+#   # Test suite for GET /steps/:id
+#   describe 'GET /steps/:id' do
+#     before { get "/steps/#{step_id}", params: {}, headers: headers }
 
-    context 'when the record exists' do
-      it 'returns the step' do
-        expect(json).not_to be_empty
-        expect(json['id']).to eq(step_id)
-      end
+#     context 'when the record exists' do
+#       it 'returns the step' do
+#         expect(json).not_to be_empty
+#         expect(json['id']).to eq(step_id)
+#       end
 
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
-    end
+#       it 'returns status code 200' do
+#         expect(response).to have_http_status(200)
+#       end
+#     end
 
-    context 'when the record does not exist' do
-      let(:step_id) { 100 }
+#     context 'when the record does not exist' do
+#       let(:step_id) { 100 }
 
-      it 'returns status code 404' do
-        expect(response).to have_http_status(404)
-      end
+#       it 'returns status code 404' do
+#         expect(response).to have_http_status(404)
+#       end
 
-      #   it 'returns a not found message' do
-      #     expect(response.body).to match(/Couldn't find step/)
-      #   end
-    end
-  end
+#       #   it 'returns a not found message' do
+#       #     expect(response.body).to match(/Couldn't find step/)
+#       #   end
+#     end
+#   end
 
-  # Test suite for POST /steps
-  describe 'POST /steps' do
-    # valid payload
-    let(:valid_attributes) do
-      { title: 'Learn Elm', day_recorded: Date.today }.to_json
-    end
+#   # Test suite for POST /steps
+#   describe 'POST /steps' do
+#     # valid payload
+#     let(:valid_attributes) do
+#       { title: 'Learn Elm', day_recorded: Date.today }.to_json
+#     end
 
-    context 'when the request is valid' do
-      before { post '/steps', params: valid_attributes, headers: headers }
+#     context 'when the request is valid' do
+#       before { post '/steps', params: valid_attributes, headers: headers }
 
-      it 'creates a step' do
-        expect(json['title']).to eq('Learn Elm')
-      end
+#       it 'creates a step' do
+#         expect(json['title']).to eq('Learn Elm')
+#       end
 
-      it 'returns status code 201' do
-        expect(response).to have_http_status(201)
-      end
-    end
+#       it 'returns status code 201' do
+#         expect(response).to have_http_status(201)
+#       end
+#     end
 
-    context 'when the request is invalid' do
-      let(:invalid_attributes) { { title: nil }.to_json }
-      before { post '/steps', params: invalid_attributes, headers: headers }
+#     context 'when the request is invalid' do
+#       let(:invalid_attributes) { { title: nil }.to_json }
+#       before { post '/steps', params: invalid_attributes, headers: headers }
 
-      it 'returns status code 422' do
-        expect(response).to have_http_status(422)
-      end
+#       it 'returns status code 422' do
+#         expect(response).to have_http_status(422)
+#       end
 
-      #   it 'returns a validation failure message' do
-      #     expect(response.body)
-      #       .to match(/Validation failed: Created by can't be blank/)
-      #   end
-    end
-  end
+#       #   it 'returns a validation failure message' do
+#       #     expect(response.body)
+#       #       .to match(/Validation failed: Created by can't be blank/)
+#       #   end
+#     end
+#   end
 
-  # Test suite for PUT /steps/:id
-  describe 'PUT /steps/:id' do
-    let(:valid_attributes) { { title: 'Shopping' }.to_json }
+#   # Test suite for PUT /steps/:id
+#   describe 'PUT /steps/:id' do
+#     let(:valid_attributes) { { title: 'Shopping' }.to_json }
 
-    context 'when the record exists' do
-      before { put "/steps/#{step_id}", params: valid_attributes, headers: headers }
+#     context 'when the record exists' do
+#       before { put "/steps/#{step_id}", params: valid_attributes, headers: headers }
 
-      it 'updates the record' do
-        expect(response.body).to be_empty
-      end
+#       it 'updates the record' do
+#         expect(response.body).to be_empty
+#       end
 
-      it 'returns status code 204' do
-        expect(response).to have_http_status(204)
-      end
-    end
-  end
+#       it 'returns status code 204' do
+#         expect(response).to have_http_status(204)
+#       end
+#     end
+#   end
 
-  # Test suite for DELETE /steps/:id
-  describe 'DELETE /steps/:id' do
-    before { delete "/steps/#{step_id}", params: {}, headers: headers }
+#   # Test suite for DELETE /steps/:id
+#   describe 'DELETE /steps/:id' do
+#     before { delete "/steps/#{step_id}", params: {}, headers: headers }
 
-    it 'returns status code 204' do
-      expect(response).to have_http_status(204)
-    end
-  end
-end
+#     it 'returns status code 204' do
+#       expect(response).to have_http_status(204)
+#     end
+#   end
+# end
