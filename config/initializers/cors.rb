1578659fff555fdc519cf67a648d49b19a7868b3
datagirl <datagirlcmr@gmail.com>
2020-08-22 20:36:31 +0100

    configure cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,22 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#     allow do
+#       origins '*'
   
-      resource '*',
-               headers: :any,
-               methods: %i[get post put patch delete options head]
-    end
+#       resource '*',
+#                headers: :any,
+#                methods: %i[get post put patch delete options head]
+#     end
+#   end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins ['http://localhost:3001', 'https://steps-tracker.herokuapp.com/']
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
+end
